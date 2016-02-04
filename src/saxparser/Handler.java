cb823d6c7cd1465bb56a9594e4f7184c2965f135
Severus256 <severus256@gmail.com>
2016-02-04 14:04:51 +0100

    Handler class modified

--- src/saxparser/Handler.java
+++ src/saxparser/Handler.java
@@ -5,6 +5,7 @@
  */
 package saxparser;
 
+import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -14,11 +15,22 @@ import org.xml.sax.helpers.DefaultHandler;
  */
 public class Handler extends DefaultHandler {
    @Override
-   public void startDocument() throws SAXException {
+   public void startDocument() throws SAXException { //срабатывает при начале документа
        System.out.println("Start parsing...");
    }
    @Override
-   public void endDocument() {
+   public void endDocument() {  // при конце документа
        System.out.println("End parsing...");
+   }
+   @Override
+   // при нахождении каждого элемента
+   // attr - это атрибут присвоенный XML элементу. Например id="1"
+   public void startElement(String namespace, String LocalName, String qName, Attributes attr) {
+       
+   }
+   @Override
+   // срабатывает при завершении чтения каждого элемента. Не принимает атрибута в параметре
+   public void endElement(String namespace, String LocalName, String qName) throws SAXException {
+       
    }
 }
